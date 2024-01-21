d2f6d9436d3b0a46e26cd1085914e3da48a74264
Rohan Kapur <kapur2001@gmail.com>
2024-01-21 06:34:15 -0800

    Made the database persistent!

--- server.js
+++ server.js
@@ -13,34 +13,57 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 // Local in-memory database initialization
 // TWO TABLES: locations for storing locations, which links to sessions table storing the current sessions
-const db = new sqlite3.Database(':memory:');
-let locID = locations.length - 1;
-let sessID = sessions.length - 1;
+const db = new sqlite3.Database('./database/db');
+const ids = {locID: 0, sessID: 0}
+
 // Example SQL join statement + multi-column foreign key: https://stackoverflow.com/a/3178747/5661257
 // SQL join statement reference: https://www.w3schools.com/sql/sql_join.asp
 // SQLite reference: https://www.tutorialspoint.com/sqlite/sqlite_insert_query.htm
 // SQL primary key ref: https://www.w3schools.com/sql/sql_primarykey.asp
 db.serialize(
   function () {
-    db.run('CREATE TABLE locations (id INT PRIMARY KEY, name VARCHAR, description VARCHAR, lat FLOAT, long FLOAT)');
-    db.run('CREATE TABLE sessions (id INT PRIMARY KEY, title VARCHAR, room VARCHAR, locationID INT REFERENCES locations(locationID))');
-    const stmt = db.prepare('INSERT INTO locations VALUES (?, ?, ?, ?, ?)');
-    for (let i = 0; i < locations.length; i++) {
-      e = locations[i];
-      stmt.run(i, e.name, e.description, e.lat, e.long);
-    }
-    stmt.finalize();
-
-    const stmt2 = db.prepare('INSERT INTO sessions VALUES (?, ?, ?, ?)');
-    for (let i = 0; i < sessions.length; i++) {
-      e = sessions[i];
-      stmt2.run(i, e.title, e.room, e.locationID);
-    }
-    stmt2.finalize();
+    db.get('SELECT MAX(id) as max FROM locations', 
+    function (err, row) {
+      if (err) {
+        db.run('CREATE TABLE IF NOT EXISTS locations (id INT PRIMARY KEY, name VARCHAR, description VARCHAR, lat FLOAT, long FLOAT)', 
+        function (err) {
+          if (err) {
+            throw err;
+          }
+          const stmt = db.prepare('INSERT INTO locations VALUES (?, ?, ?, ?, ?)');
+          for (let i = 0; i < locations.length; i++) {
+            e = locations[i];
+            stmt.run(i, e.name, e.description, e.lat, e.long);
+          }
+          stmt.finalize();
+        })
+      } else {
+        ids.locID = row.max;
+      }
+    });
+    db.get('SELECT MAX(id) AS max FROM sessions',
+    function (err, row) {
+      if (err) {
+        db.run('CREATE TABLE IF NOT EXISTS sessions (id INT PRIMARY KEY, title VARCHAR, room VARCHAR, locationID INT REFERENCES locations(locationID))',
+        function (err) {
+          if (err) {
+            throw err;
+          }
+          const stmt2 = db.prepare('INSERT INTO sessions VALUES (?, ?, ?, ?)');
+          for (let i = 0; i < sessions.length; i++) {
+            e = sessions[i];
+            stmt2.run(i, e.title, e.room, e.locationID);
+          }
+          stmt2.finalize();
+        })
+      } else {
+        ids.sessID = row.max;
+      }
+    });
   }
 );
 
-// Make a GET request to get list of all locations from the in-memory sqlite3 database
+// Make a GET request endpoint to get list of all locations from the in-memory sqlite3 database
 app.get('/locations', (req, res) => {
   db.serialize(
     function () {
@@ -71,10 +94,10 @@ app.get('/locations', (req, res) => {
 }).put('/locations', (req, res) => {
   const body = req.body;
   if (body.name && body.description && body.lat && body.long) {
-    locID += 1;
+    ids.locID += 1;
     db.serialize(
       function () {
-        db.run("INSERT INTO locations VALUES (?, ?, ?, ?, ?)", [locID, body.name, body.description, body.lat, body.long]);
+        db.run("INSERT INTO locations VALUES (?, ?, ?, ?, ?)", [ids.locID, body.name, body.description, body.lat, body.long]);
       }
     );
     res.status(201).end();
