92f1094ac81676dd4d006e768863af91d5c0c23c
Rohan Kapur <kapur2001@gmail.com>
2024-03-06 22:45:52 -0800

    Finally added changes that were need for a while LOL. Added PUT and DELETE endpoints for adding and deleting sessions, and modified README to reflect the current server API

--- README.md
+++ README.md
@@ -1,19 +1,91 @@
 # Express-Backend
+
 Express backend database server in sqlite3 for session mapping app
 
 Getting started reference: https://itnext.io/lets-dockerize-a-nodejs-express-api-22700b4105e4
 
 ## Setup
+
 To build the server image, run
+
 ```sh
 $ docker-compose build
 ```
+
 To launch the server listening on port 3000, run
+
 ```sh
 $ docker-compose up -d
 ```
+
 To stop the server
+
 ```sh
 $ docker-compose down
 ```
+
 Connect to the server via `http://localhost:3000`.
+
+This server is currently being hosted using [Google Cloud Run](https://cloud.google.com/run/docs) and can be accessed using [this link](https://express-backend-wfpd6kozoq-wl.a.run.app).
+
+## API reference
+
+This server accepts and responds to HTTP requests as follows:
+
+- `GET /locations` - This returns a JSON-formatted array of objects that each provide details for each location with the location name, description, and then it's longitude and latitude.
+  Response:
+
+```javascript
+[
+  {
+    name: string,
+    description: string,
+    long: float,
+    lat: float,
+  },
+];
+```
+
+Returns 200 upon success, or 500 upon failure.
+
+- `GET /locations/<id>` - This returns a JSON-formatted array of objects that each provide details for the sessions at the location with the `id` given. Each session currently has a title and a room number (if applicable) at the associated location.
+  Response:
+
+```javascript
+[
+  {
+    title: string,
+    room: int | null,
+  },
+];
+```
+
+Returns 200 upon success, or 500 upon failure.
+
+- `PUT /locations` - This inserts a new location into the locations table. The request body must be JSON-formatted as follows:
+
+```javascript
+  {
+    name: string,
+    description: string,
+    long: float,
+    lat: float
+  }
+```
+
+Returns 201 upon success, 400 for an ill-formatted request body, or 500 for other errors.
+
+- `PUT /locations/<id>` - This inserts a new session into the sessions table at the location given by `id`. The request body must be json-formatted as follows:
+
+```javascript
+  {
+    title: string,
+    room: int | null
+  }
+```
+
+Returns 201 upon success, 400 for an ill-formatted request body, or 500 for other errors.
+
+- `DELETE /locations/<id>` - This deletes a location with the given `id`. The response code is 200 upon success, or 500 upon failure.
+
+- `DELETE /locations/<locID>/<sessID>` - This deletes a session with the given `sessID` at the location given by `locID`. The response code is 200 upon success, or 500 upon failure.
--- server.js
+++ server.js
@@ -1,123 +1,186 @@
 /* eslint-disable */ // Disable annoying ESLint underlining
 
-const locations = require('./database/locations.json');
-const sessions = require('./database/sessions.json');
+const locations = require("./database/locations.json");
+const sessions = require("./database/sessions.json");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Local in-memory database initialization
-// TWO TABLES: locations for storing locations, which links to sessions table storing the current sessions
-const db = new sqlite3.Database('./database/db');
-const ids = {locID: locations.length, sessID: sessions.length}
+// TWO TABLES: sessions for storing session, which links to a locations table storing the current locations
+// Current locations table schema: { name, description, lat, long }
+// Current session table schema: { title, room }
+const db = new sqlite3.Database("./database/db");
+const ids = { locID: locations.length, sessID: sessions.length };
 
 // Example SQL join statement + multi-column foreign key: https://stackoverflow.com/a/3178747/5661257
 // SQL join statement reference: https://www.w3schools.com/sql/sql_join.asp
 // SQLite reference: https://www.tutorialspoint.com/sqlite/sqlite_insert_query.htm
 // SQL primary key ref: https://www.w3schools.com/sql/sql_primarykey.asp
-db.serialize(
-  function () {
-    db.get('SELECT MAX(id) as max FROM locations', 
-    function (err, row) {
-      if (err) {
-        db.run('CREATE TABLE IF NOT EXISTS locations (id INT PRIMARY KEY, name VARCHAR, description VARCHAR, lat FLOAT, long FLOAT)', 
+db.serialize(function () {
+  db.get("SELECT MAX(id) as max FROM locations", function (err, row) {
+    if (err) {
+      db.run(
+        "CREATE TABLE IF NOT EXISTS locations (id INT PRIMARY KEY, name VARCHAR, description VARCHAR, lat FLOAT, long FLOAT)",
         function (err) {
           if (err) {
             throw err;
           }
-          const stmt = db.prepare('INSERT INTO locations VALUES (?, ?, ?, ?, ?)');
+          const stmt = db.prepare(
+            "INSERT INTO locations VALUES (?, ?, ?, ?, ?)",
+          );
           for (let i = 0; i < locations.length; i++) {
-            e = locations[i];
+            const e = locations[i];
             stmt.run(i, e.name, e.description, e.lat, e.long);
           }
           stmt.finalize();
-        })
-      } else {
-        ids.locID = row.max + 1;
-      }
-    });
-    db.get('SELECT MAX(id) AS max FROM sessions',
-    function (err, row) {
-      if (err) {
-        db.run('CREATE TABLE IF NOT EXISTS sessions (id INT PRIMARY KEY, title VARCHAR, room VARCHAR, locationID INT REFERENCES locations(locationID))',
+        },
+      );
+    } else {
+      ids.locID = row.max + 1;
+    }
+  });
+  db.get("SELECT MAX(id) AS max FROM sessions", function (err, row) {
+    if (err) {
+      db.run(
+        "CREATE TABLE IF NOT EXISTS sessions (id INT PRIMARY KEY, title VARCHAR, room VARCHAR, locationID INT REFERENCES locations(locationID))",
         function (err) {
           if (err) {
             throw err;
           }
-          const stmt2 = db.prepare('INSERT INTO sessions VALUES (?, ?, ?, ?)');
+          const stmt2 = db.prepare("INSERT INTO sessions VALUES (?, ?, ?, ?)");
           for (let i = 0; i < sessions.length; i++) {
-            e = sessions[i];
+            const e = sessions[i];
             stmt2.run(i, e.title, e.room, e.locationID);
           }
           stmt2.finalize();
-        })
-      } else {
-        ids.sessID = row.max + 1;
-      }
-    });
-  }
-);
+        },
+      );
+    } else {
+      ids.sessID = row.max + 1;
+    }
+  });
+});
 
-// Make a GET request endpoint to get list of all locations from the in-memory sqlite3 database
-app.get('/locations', (req, res) => {
-  db.serialize(
-    function () {
-      db.all("SELECT * FROM locations;",
-      function (err, rows) {
+// Make the request endpoints to retrieve data from and modify the in-memory sqlite3 database
+app
+  .get("/locations", (req, res) => {
+    db.serialize(function () {
+      db.all("SELECT * FROM locations;", function (err, rows) {
         if (err) {
-          throw err;
+          res.status(500).send(`${err.name}: ${err.message}`);
         }
-        res.send(rows);
+        res.status(200).send(rows);
       });
+    });
+    // Endpoint to retrieve the list of sessions associated with a location
+  })
+  .get("/locations/:locationID", (req, res) => {
+    const params = req.params;
+    db.serialize(function () {
+      db.all(
+        "SELECT * FROM sessions WHERE locationID = ?",
+        params.locationID,
+        function (err, rows) {
+          if (err) {
+            res.status(500).send(`${err.name}, ${err.message}`);
+          }
+          res.status(200).send(rows);
+        },
+      );
+    });
+    // Endpoint to add a new location
+  })
+  // Endpoint to add a new session to a location
+  .put("/locations/:locationID", (req, res) => {
+    const body = req.body;
+    const params = req.params;
+    if (body.title && body.room) {
+      db.run(
+        "INSERT INTO sessions VALUES (?, ?, ?, ?)",
+        [ids.sessID, body.title, body.room, params.locationID],
+        function (err) {
+          if (err) {
+            res.status(500).send(`${err.name}: ${err.message}`);
+          } else {
+            ids.sessID += 1;
+            res.status(201).end();
+          }
+        },
+      );
+    } else {
+      res
+        .status(400)
+        .send(
+          "Invalid session! Sessions must be given with title and room number specifed!",
+        );
     }
-  );
-  // Endpoint to retrieve the list of sessions associated with a location
-}).get('/locations/:locationID', (req, res) => {
-  const params = req.params;
-  db.serialize(
-    function () {
-      db.all("SELECT * FROM sessions WHERE locationID = ?", params.locationID, 
-      function (err, rows) {
-        if (err) {
-          throw err;
-        }
-        res.send(rows);
-      });
+  })
+  // Endpoint to add a new location to the database.
+  .put("/locations", (req, res) => {
+    const body = req.body;
+    if (body.name && body.description && body.lat && body.long) {
+      db.run(
+        "INSERT INTO locations VALUES (?, ?, ?, ?, ?)",
+        [ids.locID, body.name, body.description, body.lat, body.long],
+        function (err) {
+          if (err) {
+            res.status(500).send(`${err.name}: ${err.message}`);
+          } else {
+            ids.locID += 1;
+            res.status(201).end();
+          }
+        },
+      );
+    } else {
+      res
+        .status(400)
+        .send(
+          "Invalid location format! Locastion must be given with name, description, long, and lat",
+        );
     }
-  )
-  // Endpoint to add a new location
-}).put('/locations', (req, res) => {
-  const body = req.body;
-  if (body.name && body.description && body.lat && body.long) {
-    db.serialize(
-      function () {
-        db.run("INSERT INTO locations VALUES (?, ?, ?, ?, ?)", [ids.locID, body.name, body.description, body.lat, body.long]);
-        ids.locID += 1;
-      }
-    );
-    res.status(201).end();
-  } else {
-    res.status(400).end();
-  }
-}).delete('/locations/:locationID', (req, res) => {
-  db.serialize(
-    function () {
-      db.run("DELETE FROM locations WHERE locationID = ?", req.params.locationID,
+  })
+  .delete("/locations/:locationID", (req, res) => {
+    db.run(
+      "DELETE FROM locations WHERE locationID = ?",
+      req.params.locationID,
       function (err) {
         if (err) {
-          res.status(500).end();
+          res.status(500).send(`${err.name}: ${err.message}`);
+        } else {
+          db.run(
+            "DELETE FROM sessions WHERE locationID = ?",
+            req.params.locationID,
+            function (err) {
+              if (err) {
+                res.status(500).send(`${err.name}: ${err.message}`);
+              } else {
+                res.status(200).end();
+              }
+            },
+          );
+        }
+      },
+    );
+  })
+  .delete("/locations/:locationID/:sessionID", (req, res) => {
+    db.run(
+      "DELETE FROM sessions WHERE locationID = ? AND sessionID = ?",
+      [req.params.locationID, req.params.sessionID],
+      function (err) {
+        if (err) {
+          res.status(500).send(`${err.name}: ${err.message}`);
         } else {
           res.status(200).end();
         }
-      });
-    }
-  )
-});
+      },
+    );
+  });
 
 // Listen on localhost port 3000 for database connections
 app.listen(port, () => {
